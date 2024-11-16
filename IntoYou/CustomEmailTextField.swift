827fb56fb534a5ba00f3233bfc2b55d8e40b28c9
michellesijiama <msijiama@gmail.com>
2024-11-16 18:52:00 -0500

    updated onboarding and signup using email

---
+++ IntoYou/CustomEmailTextField.swift
@@ -0,0 +1,18 @@
+//
+//  CustomEmailTextField.swift
+//  IntoYou
+//
+//  Created by Sijia Ma on 11/16/24.
+//
+
+import SwiftUI
+
+struct CustomEmailTextField: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CustomEmailTextField()
+}
---
+++ IntoYou/TextField.swift
@@ -0,0 +1,18 @@
+//
+//  TextField.swift
+//  IntoYou
+//
+//  Created by Sijia Ma on 11/16/24.
+//
+
+import SwiftUI
+
+struct TextField: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TextField()
+}
