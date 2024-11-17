d243dd4484497b47cff6501533240df3f8556240
michellesijiama <msijiama@gmail.com>
2024-11-16 22:35:27 -0500

    updated solved back navigation

---
+++ IntoYou/EmailVerify.swift
@@ -0,0 +1,18 @@
+//
+//  EmailVerify.swift
+//  IntoYou
+//
+//  Created by Sijia Ma on 11/16/24.
+//
+
+import SwiftUI
+
+struct EmailVerify: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EmailVerify()
+}
